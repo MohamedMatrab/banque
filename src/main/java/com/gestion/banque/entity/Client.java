da3859d041de6df3663d6908b75d93d62acd3a2e
khouyabderrazzak <abderrazzakkhouy790@gmail.com>
2024-11-28 09:37:19 +0100

    re

--- src/main/java/com/gestion/banque/entity/Client.java
+++ src/main/java/com/gestion/banque/entity/Client.java
@@ -8,7 +8,6 @@ import java.util.UUID;
 
 @Entity
 public class Client {
-
     @Id
     private String id;  // ID de type GUID (UUID)
 
@@ -17,12 +16,6 @@ public class Client {
 
     @NotBlank(message = "Le prénom est obligatoire")
     private String prenom;
-
-    @NotBlank(message = "Le login est obligatoire")
-    private String login;
-
-    @NotBlank(message = "Le mot de passe est obligatoire")
-    private String password;
 
     @NotBlank(message = "L'email est obligatoire")
     private String email;
@@ -35,12 +28,10 @@ public class Client {
     }
 
     // Constructeur personnalisé
-    public Client(String nom, String prenom, String login, String password, String email, double solde) {
+    public Client(String nom, String prenom, String email, double solde) {
         this.id = UUID.randomUUID().toString();
         this.nom = nom;
         this.prenom = prenom;
-        this.login = login;
-        this.password = password;
         this.email = email;
         this.solde = solde;
     }
@@ -68,22 +59,6 @@ public class Client {
 
     public void setPrenom(String prenom) {
         this.prenom = prenom;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getEmail() {
--- src/main/java/com/gestion/banque/entity/Role.java
+++ src/main/java/com/gestion/banque/entity/Role.java
@@ -1,9 +1,36 @@
 package com.gestion.banque.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Role {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
